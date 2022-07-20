6cc1b327dcd7d519360cb6061521bc892354e29f
Your Nameclear <you@example.com>
2022-07-20 14:36:32 +0200

    up

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ const App = () => (
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/exercise/:id" element={<ExerciseDetail />} /> */}
+      <Route path="/gym/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
     </Routes>
     <Footer />
   </Box>
