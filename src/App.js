dbca471c211eef99a744e3238ee219d94f6dc476
Your Nameclear <you@example.com>
2022-07-20 14:47:44 +0200

    up

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import './App.css';
-// import ExerciseDetail from './pages/ExerciseDetail';
+import ExerciseDetail from './pages/ExerciseDetail';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
